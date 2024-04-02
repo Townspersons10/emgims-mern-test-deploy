8b0c2ddbe1960621b92d0540ef4b64eec36ebbc8
Townspersons10 <zerogomez00@gmail.com>
2024-04-02 04:04:06 -0600

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import { PersonInventory } from './models/personInventoryModel.js'; // Importing the Book model
+import { fileURLToPath } from 'url';
 import employeeRoute from './routes/employeeRoute.js';
 import path from 'path';
 import dotenv from 'dotenv'
@@ -11,10 +12,13 @@ dotenv.config();
 
 const app = express();
 
-// Serve static files from the React app build output
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
+// Convert the file URL to a file path
+// __filename and __dirname do not exist in ES modules, so we create them
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Handle SPA routing by returning the index.html for any unknown routes
+// Then you can use __dirname as before
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html'));
 });
