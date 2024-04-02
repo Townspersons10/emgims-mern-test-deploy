59301981d145627891001ebe6a37a5cd1910c884
Townspersons10 <zerogomez00@gmail.com>
2024-04-02 03:51:00 -0600

    updated index file at backend

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import { PersonInventory } from './models/personInventoryModel.js'; // Importing the Book model
 import employeeRoute from './routes/employeeRoute.js';
 import path from 'path';
-import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
+import dotenv from 'dotenv'
 
 dotenv.config();
 
-// Define __filename and __dirname at the beginning
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Serve static files from the Vite build output directory
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
-
-// Handle SPA routing by returning the index.html file
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5555;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+
+// Attempt to connect to MongoDB
 mongoose.connect(process.env.mongoDBURL)
     .then(() => console.log('Successfully connected to MongoDB.'))
     .catch(err => console.error('Connection error', err));
+
+
 
 app.use(express.json());
 app.use(cors());
@@ -41,3 +35,11 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
+
+// Serve static files from the React frontend app in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/dist'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
